c0d5e26a56e1b415bb56ceffef92eaa9ee255857
thelamer <ryankuba@gmail.com>
2025-03-27 12:57:50 -0400

    build and error fixes

--- addons/astro-gst-web/astro.config.mjs
+++ addons/astro-gst-web/astro.config.mjs
@@ -14,36 +14,36 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
-  vite: {
-    build: {
-      chunkSizeWarningLimit: 1000,
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              if (id.includes('webrtc-adapter')) {
-                return 'webrtc/adapter';
-              }
-              if (id.includes('react') || id.includes('scheduler')) {
-                return 'vendor/react';
-              }
-              if (id.includes('@radix-ui') || id.includes('framer-motion')) {
-                return 'vendor/ui';
-              }
-              if (id.includes('zustand') || id.includes('clsx') || 
-                  id.includes('tailwind-merge')) {
-                return 'vendor/utils';
-              }
-              return 'vendor/others';
-            }
-          }
-        }
-      }
-    },
-    resolve: {
-      alias: {
-        '@': '/src',
-      },
-    }
-  }
+//  vite: {
+//    build: {
+//      chunkSizeWarningLimit: 1000,
+//      rollupOptions: {
+//        output: {
+//          manualChunks(id) {
+//            if (id.includes('node_modules')) {
+//              if (id.includes('webrtc-adapter')) {
+//                return 'webrtc/adapter';
+//              }
+//              if (id.includes('react') || id.includes('scheduler')) {
+//                return 'vendor/react';
+//              }
+//              if (id.includes('@radix-ui') || id.includes('framer-motion')) {
+//                return 'vendor/ui';
+//              }
+//              if (id.includes('zustand') || id.includes('clsx') || 
+//                  id.includes('tailwind-merge')) {
+//                return 'vendor/utils';
+//              }
+//              return 'vendor/others';
+//            }
+//          }
+//        }
+//      }
+//    },
+//    resolve: {
+//      alias: {
+//        '@': '/src',
+//      },
+//    }
+//  }
 });
